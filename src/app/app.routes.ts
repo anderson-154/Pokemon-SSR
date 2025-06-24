4d1a135b8456ed037d5f7820e465de3fc68aafa4
Anderson <andyguaca@gmail.com>
2025-06-24 13:42:02 -0500

    create the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: ()=> import('./pages/about-page/about-page.component')
+  },
+  {
+    path: 'contact',
+    loadComponent: ()=> import('./pages/contact-page copy/contact-page.component')
+  },
+  {
+    path: 'pricing',
+    loadComponent: ()=> import('./pages/pricing-page copy/pricing-page.component')
+  },
+  {
+    path: '**',
+    redirectTo: ()=> {
+      return 'about'
+    }
+  }
+];
